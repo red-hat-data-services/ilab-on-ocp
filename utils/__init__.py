dfea79c4f6f919c4ddebe0e05c5aad90214fb09f
Alberto Perdomo <aperdomo@redhat.com>
2025-06-10 15:05:22 +0100

    Only training

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,6 @@
 from .components import (
+    extract_sdg_to_pvc_op,
+    get_pvc_name_op,
     ilab_importer_op,
     model_to_pvc_op,
     pvc_to_mmlu_branch_op,
@@ -8,6 +10,8 @@ from .components import (
 )
 
 __all__ = [
+    "get_pvc_name_op",
+    "extract_sdg_to_pvc_op",
     "model_to_pvc_op",
     "pvc_to_mt_bench_op",
     "pvc_to_mt_bench_branch_op",
